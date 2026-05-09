0bf250c5c90615b047b3f829ffd291eb78525d00
Dostoyevsk1 <alexander44garfias@hotmail.com>
2026-05-09 13:22:44 -0500

    feat: add components for shared

---
+++ src/shared/presentation/layouts/main-layout.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {ref} from "vue";
+import SidebarContent from "../components/sidebar-content.vue";
+
+const drawer = ref(false);
+const toggleDrawer = () => {
+  drawer.value = !drawer.value;
+}
+
+const items = [
+  {label: 'Home', to: '/home'},
+  {label: 'option.categories', to: '/publishing/categories'},
+  {label: 'option.tutorials', to: '/publishing/tutorials'}
+];
+</script>
+
+<template>
+      <sidebar-content></sidebar-content>
+      <router-view></router-view>
+
+</template>
+
+<style scoped>
+
+.main{
+  display: flex;
+  height: 100vh;
+  width: 100vh;
+}
+</style>
